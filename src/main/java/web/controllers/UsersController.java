4ec86ee8ab52d68479535d3755559e9461516c32
PanurgRable <112486624+PanurgRable@users.noreply.github.com>
2025-08-27 23:52:16 +0300

    add validator , support russian

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -15,61 +15,74 @@ import javax.validation.Valid;
 @RequestMapping("/users")
 public class UsersController {
     private final UserServece userServece;
+
     @Autowired
     public UsersController(UserServece userServece) {
         this.userServece = userServece;
     }
+
 
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("users", userServece.index());
         return "users/index";
     }
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model){
-        model.addAttribute("user",userServece.show(id));
+
+
+    @GetMapping("/show")
+    public String show(@RequestParam("id") int id, Model model) {
+        model.addAttribute("user", userServece.show(id));
         return "users/show";
     }
-    @GetMapping("/new")
-    public String newPerson(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
-            return "users/new";
 
+    // форма добавления нового пользователя
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("user", new User());
         return "users/new";
     }
+
+
     @PostMapping()
-    public String saveUser(@ModelAttribute("user") @Valid  User user, BindingResult bindingResult){
+    public String saveUser(@ModelAttribute("user") @Valid User user,
+                           BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             return "users/new";
         userServece.save(user);
         return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
+
+    @DeleteMapping("/delete")
+    public String delete(@RequestParam("id") int id) {
         userServece.delete(id);
         return "redirect:/users";
     }
 
-    @GetMapping(value = "/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id) {
+
+    @GetMapping("/edit")
+    public String edit(Model model, @RequestParam("id") int id) {
         model.addAttribute("user", userServece.show(id));
         return "users/edit";
     }
 
-    @PostMapping("/{id}")
-    public String update(@ModelAttribute("user") @Valid User user,BindingResult bindingResult, @PathVariable("id") int id) {
+    // обновление пользователя (id передаётся как ?id=1)
+    @PostMapping("/update")
+    public String update(@ModelAttribute("user") @Valid User user,
+                         BindingResult bindingResult,
+                         @RequestParam("id") int id) {
         if (bindingResult.hasErrors())
-            return "users/new";
+            return "users/edit";
         userServece.update(id, user);
         return "redirect:/users";
     }
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
